3e8f33908b774d0b7506f2dc15e3539e01327ef3
jvhellemondt <jens.vanhellemondt@gmail.com>
2020-07-29 13:36:23 +0200

    Netlify/Stripe manage-link

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -3,7 +3,6 @@ const { faunaFetch } = require('./utils/fauna')
 
 exports.handler = async (event) => {
   const { user } = JSON.parse(event.body)
-  console.log(user)
 
   // create a new customer in Stripe
   const customer = await stripe.customers.create({ email: user.email })
